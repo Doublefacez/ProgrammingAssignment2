bdd0be3bec3ae106a63d44942ea71d8de0cc3194
Doublefacez <43196858+Doublefacez@users.noreply.github.com>
2020-06-24 23:54:54 +1000

    Completed Peer review programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+#week 3 peer assessment
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()){
+  mat <- NULL
+  set<- function(y){
+    x <<- y
+    mat <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) mat <<-inverse
+  getinverse <- function() mat
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+a <- matrix(c(1:4),  nrow = 2, ncol = 2)
+makeCacheMatrix(x=matrix(a))
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+
+cacheSolve <- function(x, ...){
+  mat <- x$getinverse()
+  
+  # if the inverse of the matrix is calculated then skip the calculation 
+ 
+  if(!is.null(mat)){
+    message("getting cached data")
+    return(mat)
+  }
+    
+  #Otherwise calculate the inverse of the matrix
+  
+  data <- x$get()
+  mat <- solve(data, ...)
+  x$setinverse(mat)   #Putting the values of the inverse calculation to a matrix
+  mat
 }
+
+
+#Testing section
+
+B <- matrix(c(4,8,12,16),2,2)
+makeCacheMatrix(B)
+cacheSolve(makeCacheMatrix(B))
